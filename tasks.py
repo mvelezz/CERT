22ca1a3f01d54181a767645ef66ba66dfe2e2fd2
mvelezz <130081489+mvelezz@users.noreply.github.com>
2024-05-06 13:12:28 -0600

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -4,6 +4,9 @@ from RPA.HTTP import HTTP
 from RPA.Tables import Tables
 from RPA.PDF import PDF
 from RPA.Archive import Archive
+from robocorp.log import exception, info
+from RPA.Browser.Selenium import Selenium
+import time
 
 
 @task
@@ -16,14 +19,12 @@ def order_robots_from_RobotSpareBin():
     Creates ZIP archive of the receipts and the images.
     """
     browser.configure(
-        slowmo=1000,
+        slowmo=1,
     )
     open_robot_order_website()
     close_pop_up()
     fill_orders()
-    collect_results()
-    fill_orders()
-    archive_receipts()
+    
     
     
 
@@ -41,31 +42,39 @@ def get_orders():
 
     library = Tables()
     orders = library.read_table_from_csv(
-        "orders.csv", columns=["Head", "Body", "Legs", "Address"]
+        "orders.csv", columns=["Order Number", "Head", "Body", "Legs", "Address"]
     )
 
     return orders
 
 def fill_the_form(robot):
+    """Fills the form with specified CSV form"""
     page = browser.page()
 
-    page.select_option('#head', str(robot["Head"]))
-    page.click('//*[@id="root"]/div/div[1]/div/div[1]/form/div[2]/div/div[{0}]/label'.format(robot["Body"]))
+    body = robot["Body"]
+    bodyTXT = f"input[value = '{body}']"
+
+    page.select_option('#head', robot["Head"])
+    page.locator(bodyTXT).click()
     page.fill("input[placeholder='Enter the part number for the legs']", robot["Legs"])
     page.fill("#address", robot["Address"])
     page.click("button:text('Preview')")
-
+    #page.click("button:text('Order')")
     while True:
         page.click("button:text('Order')")
-        #handle_alert_popup()
-        order_another_bot()
-        close_pop_up()
-    # path = store_reciept_PDF(int(robot["Order number"]))
-    # screenshot = screenshot_robot(int(robot["Order number"]))
-        #embed_screenshot_to_receipt(screenshot, path)
+        #Using query selector because if no elements match the selector, the return value resolves to null
+        order_another = page.query_selector("#order-another")
+        if order_another:
+            export_as_pdf(robot["Order Number"])
+            collect_results(robot["Order Number"])
+            order_another_bot()
+
+            close_pop_up()
+        break
 
 
 def fill_orders():
+    """Loops through the rows of csv file"""
     orders = get_orders()
 
     for row in orders:
@@ -73,48 +82,28 @@ def fill_orders():
     
 
 def close_pop_up():
+    """Closes initial pop up to waive rights"""
     page = browser.page()
     page.click("button:text('OK')")
 
-def collect_results():
+def collect_results(order_number):
     """Take a screenshot of the page"""
     page = browser.page()
+    path = f"output/{order_number}.png"
     page.screenshot(path="output/robot.png")
 
-def store_reciept_PDF(order_num):
-    page = browser.page()
-    order_HTML = page.locator("receipt").inner_html()
-    pdf = PDF()
-    path = "output/receipt/{0}.pdf".format(order_num)
-    pdf.html_to_pdf(order_HTML, path)
-    return path
-
-def screenshot_robot(order_number):
-    """Takes screenshot of the ordered bot image"""
-    page = browser.page()
-    screenshot_path = "output/screenshots/{0}.png".format(order_number)
-    page.locator("#robot-preview-image").screenshot(path=screenshot_path)
-    return screenshot_path
-
-def embed_screenshot_to_receipt(screenshot_path, pdf_path):
-    """Embeds the screenshot to the bot receipt"""
-    pdf = PDF()
-    pdf.add_watermark_image_to_pdf(image_path=screenshot_path, 
-                                   source_path=pdf_path, 
-                                   output_path=pdf_path)
     
 def order_another_bot():
     """Clicks on order another button to order another bot"""
     page = browser.page()
     page.click("#order-another")
 
+def export_as_pdf(order_number):
+    """Export the data to a pdf file"""
+    page = browser.page()
+    receipt_results_html = page.locator("#receipt").inner_html()
 
-def handle_alert_popup():
-    browser.handle_alert(action="ACCEPT")  # Accepts the alert
-    # or
-    browser.handle_alert(action="DISMISS")  # Dismisses the alert
-
-def archive_receipts():
-    """Archives all the receipt pdfs into a single zip archive"""
-    lib = Archive()
-    lib.archive_folder_with_zip("./output/receipts", "./output/receipts.zip")
+    pdf = PDF()
+    path = f"output/receipts/{order_number}.pdf"
+    pdf.html_to_pdf(receipt_results_html, path)
+    return path
